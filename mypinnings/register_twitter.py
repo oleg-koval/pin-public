8ae84e6849344a41d341e950b7888c3d96d0bf07
Camilo Torres <camilotorresf@gmail.com>
2014-03-03 21:53:13 -0430

    Ask email and manages conflicting username for twitter
    Ask email for twitter.
    When the twitter screen name collides with a username already registered, and that uername
    is not an account registered with twitter, suggest a new username and ask the user to
    confirm or select another username

--- mypinnings/register_twitter.py
+++ mypinnings/register_twitter.py
@@ -17,7 +17,9 @@ from mypinnings import auth
 
 
 urls = ('/register', 'Register',
-        '/return', 'Return')
+        '/return', 'Return',
+        '/email', 'Email',
+        '/username', 'Username',)
 
 logger = logging.getLogger('mypinnings.twitter')
 
@@ -33,13 +35,13 @@ class Register(object):
                                   )
         cb_url = web.ctx.home + '/return'
         try:
-            auth = twitter.get_authentication_tokens(callback_url=cb_url)
-            sess['oauth_token'] = auth['oauth_token']
-            sess['oauth_token_secret'] = auth['oauth_token_secret']
+            authtw = twitter.get_authentication_tokens(callback_url=cb_url)
+            sess['oauth_token'] = authtw['oauth_token']
+            sess['oauth_token_secret'] = authtw['oauth_token_secret']
         except:
             logger.error('Could not get auth tokens from twitter, review settings and twitter configuration', exc_info=True)
             return template.lmsg(_('Login to twitter not available.'))
-        raise web.seeother(url=auth['auth_url'], absolute=True)
+        raise web.seeother(url=authtw['auth_url'], absolute=True)
 
 
 class Return(object):
@@ -63,11 +65,20 @@ class Return(object):
             self.oauth_toke_secret = sess['oauth_token_secret'] = final_step['oauth_token_secret']
             if not self._get_user_credentials():
                 return template.lmsg(_('Invalid twitter login'))
-            user_id = self._get_user_from_db()
+            user_id, email = self._get_user_data_from_db()
             if not user_id:
-                user_id = self._insert_user_to_db()
-            auth.login_user(session.get_session(), user_id)
-            raise web.seeother(url='/', absolute=True)
+                self._save_profile_in_session()
+                if username_already_exists(self.credentials['screen_name']):
+                    self._suggest_a_username()
+                    raise web.seeother(url='/username', absolute=False)
+                raise web.seeother(url='/email', absolute=False)
+            if email:
+                auth.login_user(session.get_session(), user_id)
+                # username is set in _get_user_data_from_db()
+                raise web.seeother(url='/{}'.format(self.username), absolute=True)
+            else:
+                self._save_profile_in_session()
+                raise web.seeother(url='/email', absolute=False)
         else:
             logger.error('No oauth_verifyer %s', web.input())
             return template.lmsg(_("User not authenticated"))
@@ -88,19 +99,21 @@ class Return(object):
             logger.error('User not logged, could not get credentials', exc_info=True)
             return False
 
-    def _get_user_from_db(self):
+    def _get_user_data_from_db(self):
         '''
         Obtains the user_id from the database and return if exists.
         Else returns false.
         '''
         db = database.get_db()
-        query_result = db.select(tables='users', where="username=$username",
+        query_result = db.select(tables='users', where="twitter=$username and login_source=$login_source",
                                    vars={'username': self.credentials['screen_name'],
                                          'login_source': auth.LOGIN_SOURCE_TWITTER})
         for row in query_result:
             self.user_id = row['id']
-            return self.user_id
-        return False
+            self.email = row['email']
+            self.username = row['username']
+            return self.user_id, self.email
+        return False, False
 
     def _insert_user_to_db(self):
         '''
@@ -115,5 +128,116 @@ class Return(object):
         db = database.get_db()
         user_id = db.insert(tablename='users', **values)
         return user_id
+
+    def _save_profile_in_session(self):
+        sess = session.get_session()
+        if hasattr(self, 'user_id') and self.user_id:
+            sess['tw_user_id'] = self.user_id
+        else:
+            sess['tw_user_id'] = None
+        sess['tw_name'] = self.credentials['name']
+        sess['tw_username'] = self.credentials['screen_name']
+        sess['tw_twitter'] = self.credentials['screen_name']
+
+    def _suggest_a_username(self):
+        base_username = self.credentials['screen_name']
+        suggested_username = base_username + str(random.randrange(999))
+        while username_already_exists(suggested_username):
+            suggested_username = base_username + str(random.randrange(999))
+        sess = session.get_session()
+        sess['tw_username'] = suggested_username
+
+
+class Email(object):
+    '''
+    Ask for the user email; Twitter API does not provides email
+    '''
+    _email_form = web.form.Form(web.form.Textbox('email', web.form.notnull, autocomplete='off',
+                                                 id='email', placeholder=_('Where we\'ll never spam you.'),
+                                                 ),
+                                web.form.Button(_('Next')))
+
+    def GET(self):
+        form = self._email_form()
+        return template.ltpl('register/twitter/email', form)
+
+    def POST(self):
+        form = self._email_form()
+        if form.validates():
+            user_email = form['email'].value
+            sess = session.get_session()
+            user_id = sess['tw_user_id']
+            db = database.get_db()
+            if user_id:
+                db.update(tables='users', where='id=$user_id', vars={'user_id': user_id}, email=user_email)
+            else:
+                values = {'name': sess['tw_name'],
+                          'username': sess['tw_username'],
+                          'twitter':sess['tw_twitter'],
+                          'login_source': auth.LOGIN_SOURCE_TWITTER,
+                          'email': user_email,
+                          }
+                user_id = db.insert(tablename='users', **values)
+            auth.login_user(session.get_session(), user_id)
+            web.seeother(url='/{}'.format(sess.tw_username), absolute=True)
+        else:
+            return template.ltpl('register/twitter/email', form, msg=_('Please provide an email'))
+
+
+class Username(object):
+    '''
+    Ask for the user to select a username and email; Twitter API does not provides email
+    '''
+    _email_form = web.form.Form(web.form.Textbox('username', web.form.notnull, autocomplete='off',
+                                                 id='username', placeholder=_('Select a username for your profile.'),
+                                                 ),
+                                web.form.Textbox('email', web.form.notnull, autocomplete='off',
+                                                 id='email', placeholder=_('Where we\'ll never spam you.'),
+                                                 ),
+                                web.form.Button(_('Next')))
+
+    def GET(self):
+        sess = session.get_session()
+        form = self._email_form()
+        form['username'].set_value(sess.tw_username)
+        return template.ltpl('register/twitter/username', form)
+
+    def POST(self):
+        form = self._email_form()
+        if form.validates():
+            self.email = form['email'].value
+            self.username = form['username'].value
+            if username_already_exists(self.username):
+                return template.ltpl('register/twitter/username', form, msg=_('Username already exists'))
+            self._insert_user_to_db()
+            auth.login_user(session.get_session(), self.user_id)
+            web.seeother(url='/{}'.format(self.username), absolute=True)
+        else:
+            return template.ltpl('register/twitter/username', form, msg=_('Please provide an username and email'))
+
+    def _insert_user_to_db(self):
+        sess = session.get_session()
+        values = {'name': sess['tw_name'],
+                  'username': self.username,
+                  'twitter': sess['tw_twitter'],
+                  'login_source': auth.LOGIN_SOURCE_TWITTER,
+                  'email': self.email,
+                  }
+        db = database.get_db()
+        self.user_id = db.insert(tablename='users', **values)
+        return self.user_id
+
+
+def username_already_exists(username):
+    '''
+    Test if the username already exists in the DB
+    '''
+    db = database.get_db()
+    query_result = db.select(tables='users', where="username=$username",
+                               vars={'username': username})
+    for _ in query_result:
+        return True
+    return False
+
 
 app = web.application(mapping=urls, fvars=locals())
