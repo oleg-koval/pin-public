7d0c48753dc953b8b8b30a77b3020a537ce9c2ae
Camilo Torres <camilotorresf@gmail.com>
2014-03-30 11:48:05 -0430

    In discover menu show only root categories

--- mypinnings/cached_models.py
+++ mypinnings/cached_models.py
@@ -3,4 +3,5 @@ all_categories = None
 
 def initialize(db):
     global all_categories
-    all_categories = list(db.select('categories', order='id'))
+    all_categories = list(db.select('categories', order='id',
+                                    where='parent is null'))
