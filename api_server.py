a4e853d2c8de76a39a24f52402a1153b63679c74
Igor Pochechuev <pochechuev.igor@djangostars.com>
2014-04-11 12:32:00 +0300

    Dev server loading added

--- api_server.py
+++ api_server.py
@@ -2,7 +2,8 @@
 import web
 import logging
 
-from api.views.notifications import Notification
+import api.views.base
+import api.views.notifications
 
 
 class redirect:
@@ -13,12 +14,11 @@ class redirect:
 		web.seeother('/' + path)
 
 urls = (
-	"/(.*)/", redirect, # Handle urls with slash and without it
-	"/query/notification", Notification, # API handler for notifications
+	"/(.*)/", 'redirect', # Handle urls with slash and without it
+	"/query/notification", api.views.notifications.Notification, # API handler for notifications
 )
-
 web.config.debug = True
 app = web.application(urls, globals(), autoreload=True)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	app.run()
---
+++ reload.sh
@@ -0,0 +1,2 @@
+/usr/bin/touch /Users/igorpochechuev/Development/spoontch/pin/api_server.py;
+/usr/bin/touch /Users/igorpochechuev/Development/spoontch/pin/ser.py;
