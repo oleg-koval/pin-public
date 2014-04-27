59fdcca5dc902aa3541a5105ac3b7c912de559ce
Camilo Torres <camilotorresf@gmail.com>
2014-04-27 07:42:37 -0430

    S3 images names only lowercase and + numbers

--- mypinnings/media.py
+++ mypinnings/media.py
@@ -7,7 +7,7 @@ import boto.s3.key
 from PIL import Image
 
 
-NAME_CHARACTERS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-._'
+NAME_CHARACTERS = 'abcdefghijklmnopqrstuvwxyz1234567890123456789012345678901234567890'
 
 
 def store_image_from_filename(db, filename, widths=[]):
