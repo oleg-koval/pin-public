05b8ab032c70133e51a2b61d1b13f91ef078fbd7
Igor Pochechuev <pochechuev.igor@djangostars.com>
2014-05-23 22:56:34 +0300

    More button

--- ser.py
+++ ser.py
@@ -157,6 +157,7 @@ urls = (
     '/recover_password_sent/?', 'mypinnings.recover_password.EmailSentPage',
     '/pwreset/(\d*)/(\d*)/(.*)/', 'mypinnings.recover_password.PasswordReset',
     '/recover_password_complete/', 'mypinnings.recover_password.RecoverPasswordComplete',
+    '/getuserpins/(.*?)', 'GetUserPins', 
     '/(.*?)', 'PageProfile2',
     '/(.*?)/(.*?)', 'PageConnect2',
 
@@ -927,6 +928,38 @@ class PageConnect2:
             else:
                 return 'Page not found'
         return ltpl('connect2',user, follows, followers, friends, action)
+
+
+class GetUserPins:
+    def GET(self, username):
+        force_login(sess)
+        # logintoken = convert_to_logintoken(sess.user_id)
+        # data = {"csid_from_client": ""}
+
+        # # Getting profile of a given user
+        # profile_url = "/api/profile/userinfo/info"
+        # profile_owner_context = {
+        #     "csid_from_client": "",
+        #     "username": username,
+        #     "logintoken": logintoken}
+        # user = api_request(profile_url, data=profile_owner_context)\
+        #     .get("data", [])
+        # # import ipdb; ipdb.set_trace()
+        # if len(user) == 0:
+        #     return u"Profile was not found"
+
+        # user = pin_utils.dotdict(user)
+        # offset = int(web.input(offset=1).offset)
+
+        # # Updating api_request data with user_id
+        # data['user_id'] = user.id
+        # data['page'] = offset
+        # data['items_per_page'] = PIN_COUNT
+        # pins_api_response = api_request("/api/profile/userinfo/pins", data=data)
+        # pins = pins_api_response.get('data').get('pins_list')
+
+        # return ltpl('getmorepins.html')
+        return ltpl('index.html')
 
 
 class PageProfile2:
