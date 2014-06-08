8dfc1891317dfbc3d60fdfd81b019a22049313d6
Camilo Torres <camilotorresf@gmail.com>
2014-06-08 04:49:51 -0430

    In category listing: load 50 items at a time

--- settings.py
+++ settings.py
@@ -56,7 +56,7 @@ COUNTRIES = [
     'Virgin Islands, British', 'Virgin Islands, U.s.', 'Wallis and Futuna', 'Western Sahara', 'Yemen', 'Zambia', 'Zimbabwe',
 ]
 
-PIN_COUNT = 30
+PIN_COUNT = 50
 SEARCH_PINS = True
 
 API_URL = os.environ['API_URL'] if 'API_URL' in os.environ else "http://mypinnings.com/"
